3e0257ed98c3e16438da16e97a9852368d201526
Osama <Osamamu64@gmail.com>
2024-03-11 15:50:29 +0400

    Investment Caclulator initial setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div id="header">
+      <img src="investment-calculator-logo.png" alt="Investment Claculator Logo" />
+      <h1>React Investment Calculator</h1>
+    </div>
+  )
+}
+
+export default App
