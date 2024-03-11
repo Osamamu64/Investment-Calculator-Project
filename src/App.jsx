f71b5d3b0bc3523891178c534d627ea7ad63bd44
Osama <Osamamu64@gmail.com>
2024-03-11 17:05:29 +0400

    Refactored header, and added user-input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+
+import UserInput from "./components/UserInput"
+import Header from "./components/Header"
+
+
 function App() {
   return (
-    <div id="header">
-      <img src="investment-calculator-logo.png" alt="Investment Claculator Logo" />
-      <h1>React Investment Calculator</h1>
-    </div>
+    <>
+      <Header />
+      <UserInput />
+    </>
   )
 }
 
